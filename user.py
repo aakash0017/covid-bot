fa5a69901716c42d4359a3c9e92c742a7ea059f0
Nidhir Bhavsar <nidbhavsar989@gmail.com>
2021-04-26 16:17:00 +0530

    Added modification to user.py also created user_utility directory and enhanced utility/_utility replica of utility/utility

--- user.py
+++ user.py
@@ -25,6 +25,9 @@ class user:
         # resource file path 
         self.file_path = 'data/res.npy'
 
+        # tokens
+        tokens = ['state', 'city', 'resources']
+
         # load resource list saved under data/ directory
         # self.load_res()
         # initialize resource dict
@@ -36,9 +39,6 @@ class user:
     def help_required(self):
         self.aid = True
 
-    # def load_res(self):
-    #     self.resource = list(np.load('data/res.npy', allow_pickle=True))
-
     def create_res_dict(self):
         # list of zeros of same lenght as of resources
         self.resource = user_utility.load_file(self.file_path)
@@ -46,7 +46,7 @@ class user:
         self.res_dict = dict(zip(self.resource, indicator))
 
     def get_details(self):
-        details = [self.name, self.email_Id, self.mobile, self.helper, self.aid]
+        details = [self.name, self.email_Id, self.mobile, self.helper, self.aid, self.state, self.city]
         return details,self.resource, self.res_dict
 
     def mapping(self):
@@ -55,9 +55,20 @@ class user:
             if value == 1:
                 res_available.append(key)
 
+    def update_attributes(self, token, update_key):
+        if token == 'state':
+            self.state = update_key
+        elif token == 'city':
+            self.city = update_key
+        elif token == 'resources':
+            self.res_dict =  user_utility.update_dict(self.res_dict, update_key)
+
 if __name__ == "__main__":
     
     user1 = user('nidhir', 'nid989@nid.com', '123456789')
     user1.resource_provider()
     lst, res, x = user1.get_details()
-    print(res)
+    print(lst)
+    user1.update_attributes('city', 'Vadodara')
+    lst, res, x = user1.get_details()
+    print(lst)
---
+++ utility/_utility.py
@@ -0,0 +1,66 @@
+import numpy as np
+import enchant
+
+
+def take_input(input_type):
+    # temporary user_input
+    user_input = 'Oximater'
+    user_input = user_input.lower()
+    # handle input_type = 'int'
+    processed_input = check_validity(user_input, input_type)
+    return processed_input
+
+def check_validity(user_input, input_type):
+
+    sim_input, status = most_similar(user_input, input_type)
+    if status == True:
+        possible_inputs = handle_invalid_input(user_input, input_type)
+        return [status, possible_inputs]
+    else:
+        return sim_input
+
+# universal invalid user input handler 
+def handle_invalid_input(user_input, input_type):
+    k = 5
+    if input_type == 'city':
+        objective = load_cities()
+    elif input_type == 'res':
+        objective = load_resources()
+    scores = []
+    # calculate distance for all cities
+    for content in objective:
+        distance = enchant.utils.levenshtein(user_input, content)
+        scores.append(distance)
+    # convert to numpy array
+    scores = np.array(scores)
+    possible_inputs = objective[np.argpartition(scores, k)[:k]]
+    return list(possible_inputs)
+
+# universal similarity checker for both city and resource input type
+def most_similar(user_input, input_type):
+    if input_type == 'city':
+        objective = load_cities()
+    elif input_type == 'res':
+        objective = load_resources()
+    status = False
+    scores = []
+    # calculate score b/w contents in objective list and user_input
+    for content in objective:
+        distance = enchant.utils.levenshtein(user_input, content)
+        scores.append(distance)
+    # convert to numpy array
+    scores = np.array(scores)
+    min_idx = scores.argmin()
+    if scores[min_idx] > 3:
+        status = True
+    sim_obj = objective[min_idx]
+    return sim_obj, status
+
+
+def load_cities():
+    return np.load('../data/cities.npy', allow_pickle=True)
+
+def load_resources():
+    return np.load('../data/res.npy', allow_pickle=True)
+
+print(take_input('res'))
