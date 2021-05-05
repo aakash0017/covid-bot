0e7344490289af1804aec9707c2874ea1b98adfa
aakash0017 <akashbharat.bhatnagar@gmail.com>
2021-05-05 14:40:13 +0530

    minor changes

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def index():
     if request.method == "POST":
         msg = request.get_json()
         
-         try:
+        try:
             txt = msg["message"]["text"]
         except:
             write_json(msg, 'telegram_request.json')
